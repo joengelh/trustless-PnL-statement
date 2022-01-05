6e96c0c9284356967ac9d68d79b542868d97f83d
joengelh <73387330+joengelh@users.noreply.github.com>
2022-01-05 14:48:47 +0100

    automated commit for update saving

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,6 @@ import getConfig from './config'
 
 const nearConfig = getConfig(process.env.NODE_ENV || 'development')
 
-console.log(nearConfig)
-
 // Initialize contract & set global variables
 export async function initContract() {
   // Initialize connection to the NEAR testnet
@@ -20,9 +18,9 @@ export async function initContract() {
   // Initializing our contract APIs by contract name and configuration
   window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
     // View methods are read only. They don't modify the state, but usually return some value.
-    viewMethods: ['get_greeting'],
+    viewMethods: ['get_fund_name'],
     // Change methods can modify the state. But you don't receive the returned value when called.
-    changeMethods: ['set_greeting'],
+    changeMethods: ['set_fund_name'],
   })
 }
 
