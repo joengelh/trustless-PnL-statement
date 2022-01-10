12f83218b48023aab3b68592c12245351814eea3
joengelh <73387330+joengelh@users.noreply.github.com>
2022-01-10 13:44:27 +0100

    automated commit for update saving

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,10 @@ console.log(nearConfig)
 // Initialize contract & set global variables
 export async function initContract() {
   // Initialize connection to the NEAR testnet
-  const near = await connect(Object.assign({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, nearConfig))
+  const near = await connect(Object.assign({ 
+    deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } }, 
+    nearConfig)
+    )
 
   // Initializing Wallet based Account. It can work with NEAR testnet wallet that
   // is hosted at https://wallet.testnet.near.org
@@ -18,11 +21,12 @@ export async function initContract() {
   window.accountId = window.walletConnection.getAccountId()
 
   // Initializing our contract APIs by contract name and configuration
-  window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
-    // View methods are read only. They don't modify the state, but usually return some value.
-    viewMethods: ['get_greeting'],
-    // Change methods can modify the state. But you don't receive the returned value when called.
-    changeMethods: ['set_greeting'],
+  window.contract = await new Contract(window.walletConnection.account(), 
+    nearConfig.contractName, {
+      // View methods are read only. They don't modify the state, but usually return some value.
+      viewMethods: ['get_pnl'],
+      // Change methods can modify the state. But you don't receive the returned value when called.
+      changeMethods: ['add_statement'],
   })
 }
 
